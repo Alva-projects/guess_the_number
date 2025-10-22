13bc4e5e7f1e6f730f0e6ef80d9f90553bc3bc9c
Alva-projects <alva.lundberg@hotmail.se>
2025-10-22 11:14:40 +0200

    first commit, with code ideas

---
+++ index.js
@@ -0,0 +1,8 @@
+const  target = Math.floor(Math.random()*10+1); 
+let guess = null;
+    if guess = unfedined("Please have a guess");
+    if guess = target
+    if guess > prompt ('Guess lower!')
+    if guess < prompt('Guess higher!')
+    while guess = null
+    console.log('You guessed the right number!')
