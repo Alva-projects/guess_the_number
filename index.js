09685021ca77791289b67a05231004ce9ba3ccaf
Alva-projects <alva.lundberg@hotmail.se>
2025-10-22 13:08:18 +0200

    Number generator works

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const  target = Math.floor(Math.random()*10+1); 
+const target = Math.floor(Math.random()*10+1); 
+console.log(target);
+
+let guess;
+/*
+
 let guess = null;
+console.log(target);
+
+/*
     if guess = unfedined("Please have a guess");
     if guess = target
     if guess > prompt ('Guess lower!')
     if guess < prompt('Guess higher!')
     while guess = null
     console.log('You guessed the right number!')
+*/
