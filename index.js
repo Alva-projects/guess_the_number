9ff7c06de33f9a38ad90a46c48937341af492945
Alva-projects <alva.lundberg@hotmail.se>
2025-10-23 10:10:30 +0200

    while guess !== target

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ console.log(target);
 let guess;
 let running = true;
 /*
-
+while guess !== target
 let guess = null;
 console.log(target);
 
 /*
     if guess = unfedined("Please have a guess");
     if guess = target
-    if guess > prompt ('Guess lower!')
-    if guess < prompt('Guess higher!')
+    if guess > target; prompt ('Guess lower!')
+    if guess <  target; prompt('Guess higher!')
     while guess = null
     console.log('You guessed the right number!')
 */
