293cbba123427fd8ed96205c16b6b0275968a702
Alva-projects <alva.lundberg@hotmail.se>
2025-10-25 22:04:47 +0200

    Final version

--- index.js
+++ index.js
@@ -4,29 +4,11 @@ let guess = null;
 while (guess !== target) {
     console.log(`Guess: ${guess}`);
     guess = parseFloat(prompt(`What number am I thinking of?`));
+    if (guess > target) {
+        console.log("Lower");
+    } else if (guess < target) {
+        console.log("Higher");
+    } else {
+        console.log(`Congratulations! You managed to guess the correct number!`); 
+    }
 }
-if (guess > target) {
-guess = parseFloat(prompt(`Lower`));
-} else if {
-parseFloat(prompt(`Lower`));
-else {
-    console.log(`Congratulations! You managed to guess the correct number!`); 
-}
-}
-    
-
-/*
-Target: ${target}.
-console.log('Incorrect');
-if (guess === target)
-console.log('Correct');
-let guess = null;
-console.log(target);
-
-/*
-    if guess = unfedined("Please have a guess");
-    if guess = target
-    if guess <  target; prompt('Guess higher!')
-    while guess = null
-    console.log('You guessed the right number!')
-*/
