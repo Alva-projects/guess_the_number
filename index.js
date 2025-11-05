9f83f359fe20cd473c434fe9711135f045519535
Alva-projects <alva.lundberg@hotmail.se>
2025-11-05 09:11:16 +0100

    added inside a function and it logs better now because console.log() is added.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //This is my random number guessing game between 1-10
 const target = Math.floor(Math.random()*10+1); 
 let guess = null;
-
-while (guess !== target) {
-    console.log(`Guess: ${guess}`);
-    guess = parseFloat(prompt(`What number am I thinking of?`));
-    if (guess > target) {
-        console.log("Lower");
-    } else if (guess < target) {
-        console.log("Higher");
-    } else {
-        console.log(`Congratulations! You managed to guess the correct number!`); 
+function guessingGame() {
+    while (guess !== target) {
+        guess = parseFloat(prompt(`What number am I thinking of?`));
+        if (guess > target) {
+            console.log(`Guess: ${guess}`);
+            console.log("Lower");
+        } else if (guess < target) {
+            console.log(`Guess: ${guess}`);
+            console.log("Higher");
+        } else {
+            console.log(`Congratulations! You managed to guess the correct number!`); 
+        }
     }
 }
+guessingGame();
